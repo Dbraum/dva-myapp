06dd507dd048dc5cb5d96d29760963092a928bdd
MAC_CN <757246444@qq.com>
2017-01-05 13:10:25 +0800

    Webpack hook event to write html file into /server/views due to server render

--- server/server.dev.js
+++ server/server.dev.js
@@ -2,6 +2,7 @@ import express from 'express';
 import webpack from 'webpack';
 import config from '../webpack/webpack.dev.config';
 import path from 'path'
+import fs from 'fs'
 const app = express();
 
 // webpack compile
@@ -19,13 +20,31 @@ const options = {
   },
 };
 
+// Webpack hook event to write html file into /server/views due to server render
+compiler.plugin('emit', (compilation, callback) => {
+    const assets = compilation.assets
+    let file, data
+
+    Object.keys(assets).forEach(key => {
+        if (key.match(/\.html$/)) {
+            file = path.resolve(__dirname, key)
+            data = assets[key].source()
+            fs.writeFileSync(file, data)
+        }
+    })
+    callback()
+})
+
 app.use(require('webpack-dev-middleware')(compiler, options));
 app.use(require('webpack-hot-middleware')(compiler));
+
+
+
 //app.use(require('./ssrMiddleware'));
 app.disable('x-powered-by');
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, '../src', 'index.html'))
+  response.sendFile(path.resolve(__dirname, '../views/dev', 'index.html'))
 })
 
 const server = app.listen(8989, () => {
