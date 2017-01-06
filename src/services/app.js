f572aeef290dc1a7ebd16e48bacae1818f330c99
MAC_CN <757246444@qq.com>
2017-01-07 00:01:47 +0800

    调整登录逻辑

--- src/services/app.js
+++ src/services/app.js
@@ -1,22 +1,36 @@
-import { request } from '../utils'
+import {fetch} from '../utils'
 
-export async function login (params) {
-  return request('/api/login', {
+export async function login(params) {
+  // return fetch('/api/login', {   method: 'post',   body: params })
+  const {username, password} = params;
+  const response = {};
+  if (username === 'guest' && password === 'guest') {
+
+    const now = new Date()
+    now.setDate(now.getDate() + 1)
+    localStorage.setItem('user_session', now.getTime())
+    localStorage.setItem('user_name', username)
+    response.message = '登录成功'
+    response.success = true
+    localStorage
+  } else {
+    response.message = "账号或者密码不正确"
+  }
+  return response
+}
+
+export async function logout(params) {
+  return fetch('/api/logout', {
     method: 'post',
-    data: params
+    body: params
   })
 }
 
-export async function logout (params) {
-  return request('/api/logout', {
-    method: 'post',
-    data: params
-  })
-}
-
-export async function userInfo (params) {
-  return request('/api/userInfo', {
-    method: 'get',
-    data: params
-  })
+export async function userInfo() {
+  // return fetch('/api/userInfo', {   method: 'get' })
+  return {
+    success: localStorage.getItem('user_session') && localStorage.getItem('user_session') > new Date().getTime(),
+    username: localStorage.getItem('user_name') || '',
+    message: ''
+  }
 }
