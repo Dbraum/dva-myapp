a716d8bb594f5447dc12a2780975d21cb2826e7f
MAC_CN <757246444@qq.com>
2017-01-20 17:01:38 +0800

    babel-plugin-add-module-exports fix

--- src/router.js
+++ src/router.js
@@ -2,45 +2,48 @@ import React from 'react'
 import {Router} from 'dva/router'
 import App from './routes/app'
 
-export default function ({history, app}) {
-  const routes = [
-    {
-      path: '/',
-      component: App,
-      getIndexRoute (nextState, cb) {
-        require.ensure([], require => {
-          cb(null, {component: require('./routes/users')})
-        })
-      },
-      childRoutes: [
-        {
-          path: 'dashboard',
-          name: 'dashboard',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/dashboard'))
-            })
-          }
-        }, {
-          path: 'users',
-          name: 'users',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/users'))
-            })
-          }
-        }, {
-          path: '*',
-          name: 'error',
-          getComponent (nextState, cb) {
-            require.ensure([], require => {
-              cb(null, require('./routes/error'))
-            })
-          }
+
+export const routes = [
+  {
+    path: '/',
+    component: App,
+    getIndexRoute (nextState, cb) {
+      require.ensure([], require => {
+        cb(null, {component: require('./routes/users')})
+      })
+    },
+    childRoutes: [
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        getComponent (nextState, cb) {
+          require.ensure([], require => {
+            cb(null, require('./routes/dashboard'))
+          })
         }
-      ]
-    }
-  ]
+      }, {
+        path: 'users',
+        name: 'users',
+        getComponent (nextState, cb) {
+          require.ensure([], require => {
+            cb(null, require('./routes/users'))
+          })
+        }
+      }, {
+        path: '*',
+        name: 'error',
+        getComponent (nextState, cb) {
+          require.ensure([], require => {
+            cb(null, require('./routes/error'))
+          })
+        }
+      }
+    ]
+  }
+]
+
+export default function ({history, app}) {
+
   return <Router history={history} routes={routes} />
 };
 
